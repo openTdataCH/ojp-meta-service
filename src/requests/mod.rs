6baf0f86567c78a1bf135fcacec69f505a86bd1c
laurens <laurens.inauen@hkb.bfh.ch>
2022-11-10 20:59:28 +0100

    format exchange point request

--- src/requests/mod.rs
+++ src/requests/mod.rs
@@ -27,3 +27,26 @@ pub fn format_lir(query: &str, nr_of_results: usize, include_pt_modes: bool) -> 
   </OJP>"#
     )
 }
+
+pub fn format_epr(requestor_ref: &str) -> String {
+    let timestamp =
+        DateTime::<Utc>::from(SystemTime::now()).to_rfc3339_opts(SecondsFormat::Millis, true);
+    format!(
+        r#"<?xml version="1.0" encoding="UTF-8"?>
+    <OJP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.siri.org.uk/siri" version="1.0" xmlns:ojp="http://www.vdv.de/ojp" xsi:schemaLocation="http://www.siri.org.uk/siri ../ojp-xsd-v1.0/OJP.xsd">
+        <OJPRequest>
+            <ServiceRequest>
+                <RequestTimestamp>{timestamp}</RequestTimestamp>
+                <RequestorRef>{requestor_ref}</RequestorRef>
+                <ojp:OJPExchangePointsRequest>
+                    <RequestTimestamp>{timestamp}</RequestTimestamp>
+                       <ojp:Params>
+                            <ojp:NumberOfResults>100</ojp:NumberOfResults>
+                            <ojp:ContinueAt>0</ojp:ContinueAt>
+                       </ojp:Params>
+                </ojp:OJPExchangePointsRequest>
+            </ServiceRequest>
+        </OJPRequest>
+    </OJP>"#
+    )
+}
